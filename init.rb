b5bc6692c57ec551d97ca06695cc1b8d6cc9e0f5
Kitagawa_Satoshi <kitagawa.satoshi.register+github@gmail.com>
2018-05-18 01:59:58 +0900

    fix typo.

--- init.rb
+++ init.rb
@@ -12,7 +12,7 @@ Redmine::Plugin.register :mega_calendar do
   permission :view_mega_calendar, { :calendar => [:get_saved_filters, :query_filter, :index, :form_holiday, :form_issue, :get_events] }
   permission :edit_mega_calendar, { :calendar => [:save_filters, :destroy_filter, :export, :change_holiday, :change_issue] }
   permission :view_holidays, { :holidays => [:index, :show] }     
-  permission :edit_holydays, { :holidays => [:new, :create, :edit, :update, :destory] }
+  permission :edit_holydays, { :holidays => [:new, :create, :edit, :update, :destroy] }
   menu(:top_menu, :calendar, { :controller => 'calendar', :action => 'index' }, :caption => :calendar )
   menu(:top_menu, :holidays, { :controller => 'holidays', :action => 'index' }, :caption => :holidays )
   settings :default => {'display_empty_dates' => 0, 'displayed_type' => 'users', 'displayed_users' => User.where(["users.login IS NOT NULL AND users.login <> ''"]).collect {|x| x.id.to_s}, 'default_holiday_color' => 'D59235', 'default_event_color' => '4F90FF', 'sub_path' => '/', 'week_start' => '1', 'allowed_users' => User.where(["users.login IS NOT NULL AND users.login <> ''"]).collect {|x| x.id.to_s}}, :partial => 'settings/mega_calendar_settings'
